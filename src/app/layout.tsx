93aa2f298b185a0674582176256926aff88942ef
edwardyh80 <edwardyh80@gmail.com>
2023-10-15 03:39:26 +0900

    analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import type { ReactNode } from "react";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 
+import { Analytics } from "@vercel/analytics/react";
+
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -14,7 +16,10 @@ export const metadata: Metadata = {
 
 const RootLayout = ({ children }: { children: ReactNode }) => (
   <html lang="en">
-    <body className={inter.className}>{children}</body>
+    <body className={inter.className}>
+      {children}
+      <Analytics />
+    </body>
   </html>
 );
 
